3d271ce207ba2d3f4855d7ba2236b1da276e5569
Pujari Balaji <119519643+pujaribalaji@users.noreply.github.com>
2026-05-10 01:50:50 +0530

    Update api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE ?? "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_BASE ?? "https://aarinkabackend.vercel.app";
 
 export const api = axios.create({
   baseURL: API_BASE
